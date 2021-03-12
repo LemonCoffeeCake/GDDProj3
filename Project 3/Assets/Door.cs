452200fd8618bb752bf005cd85a858783d519035
Max Bograd <max.bograd@gmail.com>
2021-03-11 16:35:54 -0800

    door MVP finished

---
+++ Project 3/Assets/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+
+    private GameObject[] enemies;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+         if (coll.CompareTag("Player"))
+         {
+             enemies = GameObject.FindGameObjectsWithTag("Enemy");
+             if (enemies.Length == 0) {
+                MoveRoom();
+            }
+         }
+    }
+
+    private void MoveRoom() {
+        SceneManager.LoadScene("Level1");
+    }
+}
