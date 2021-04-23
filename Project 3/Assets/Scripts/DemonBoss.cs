dd60d57d1c5e68f2f74552355cba5bbe5390fa49
Brandon Htet <bhtet@berkeley.edu>
2021-04-23 13:03:58 -0700

    changed demon to archer

--- Project 3/Assets/Scripts/DemonBoss.cs
+++ Project 3/Assets/Scripts/DemonBoss.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DemonBoss : EnemyController
+public class DemonBoss : ArcherController
 {
     #region Editor Variables
     [SerializeField]
